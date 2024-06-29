7855497946c743f994d0e04afcd3b93acdca4e8b
sanad-bhowmik <sanad15-2952@diu.edu.bd>
2024-06-29 18:00:58 +0600

    Send sms To live mobile Number Done on 29/06/2024 6:00PM

--- sms.php
+++ sms.php
@@ -1,5 +1,5 @@
 <?php
-echo "test sms";
+//echo "test sms";
 
 $keyword = isset($_GET['keyword']) ? $_GET['keyword'] : '';
 $msisdn = isset($_GET['msisdn']) ? $_GET['msisdn'] : '';
@@ -8,12 +8,14 @@ $msgid = isset($_GET['msgid']) ? $_GET['msgid'] : '';
 $telcoid = isset($_GET['telcoid']) ? $_GET['telcoid'] : '';
 $shortcode = isset($_GET['shortcode']) ? $_GET['shortcode'] : '';
 $datetime = isset($_GET['datetime']) ? $_GET['datetime'] : '';
-var_dump($keyword);
+//var_dump($keyword);
+
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "smspanelnew";
-
+$date = date('Y-m-d');
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
@@ -28,9 +30,37 @@ $result = $sql->get_result();
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         echo $row['sms'];
+if($telcoid==3){
+$registerMO_url='http://103.228.39.36/smppsend/blsmpp_test.php';
+$registerMO_param="?msisdn=$msisdn&message=".urlencode($row['sms'])."";
+$ftp222 =fopen("log/contentsms_".$date.".txt",'a+');
+        fwrite($ftp222,$registerMO_url."?".$registerMO_param."-".$datetime."\n");
+        fclose($ftp222);
+$response	=HttpRequest($registerMO_url,$registerMO_param);	
+}		
+
+
+		
     }
 } else {
     echo "No SMS found for the keyword: " . htmlspecialchars($keyword);
 }
 
 $conn->close();
+
+
+
+
+function HttpRequest($url,$param) { 
+    $URL_STR =$url.$param;
+    $ch=curl_init();
+    curl_setopt($ch,CURLOPT_URL,$URL_STR);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,false);
+    curl_exec($ch);
+    $response = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    return $response;
+} 
+
+
+?>
