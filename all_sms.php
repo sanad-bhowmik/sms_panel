fcbfb11aafdb6b4803f179ee7c5ca5c8135c530b
sanad-bhowmik <sanad15-2952@diu.edu.bd>
2024-06-29 17:52:18 +0600

    Send sms To live mobile Number Done on 29/06/2024

--- all_sms.php
+++ all_sms.php
@@ -24,96 +24,96 @@ $keywordsResult = mysqli_query($connection, $keywordsQuery);
 mysqli_close($connection);
 ?>
 <style>
-.dropdown {
-    position: relative;
-    display: inline-block;
-    margin-right: 30px;
-    margin-top: 20px;
-}
+    .dropdown {
+        position: relative;
+        display: inline-block;
+        margin-right: 30px;
+        margin-top: 20px;
+    }
 
-.dropdown-button {
-    background-color: #35A29F;
-    color: white;
-    padding: 10px;
-    border: none;
-    cursor: pointer;
-}
+    .dropdown-button {
+        background-color: #35A29F;
+        color: white;
+        padding: 10px;
+        border: none;
+        cursor: pointer;
+    }
 
-.dropdown-content {
-    display: none;
-    position: absolute;
-    background-color: #f9f9f9;
-    min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-    z-index: 1;
-}
+    .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: #f9f9f9;
+        min-width: 160px;
+        box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+        z-index: 1;
+    }
 
-.dropdown-content a {
-    color: black;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-}
+    .dropdown-content a {
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+    }
 
-.dropdown-content a:hover {
-    background-color: #E2F6CA;
-}
+    .dropdown-content a:hover {
+        background-color: #E2F6CA;
+    }
 
-.dropdown:hover .dropdown-content {
-    display: block;
-}
+    .dropdown:hover .dropdown-content {
+        display: block;
+    }
 
-.table {
-    border-collapse: collapse;
-    width: 100%;
-    margin-bottom: 20px;
-    background-color: #ffffff;
-    box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
-}
+    .table {
+        border-collapse: collapse;
+        width: 100%;
+        margin-bottom: 20px;
+        background-color: #ffffff;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+    }
 
-.table th,
-.table td {
-    padding: 12px 15px;
-    border-bottom: 1px solid #dee2e6;
-    text-align: left;
-}
+    .table th,
+    .table td {
+        padding: 12px 15px;
+        border-bottom: 1px solid #dee2e6;
+        text-align: left;
+    }
 
-.table th {
-    background: rgb(0, 27, 36);
-    background: radial-gradient(circle, rgba(0, 27, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(11, 7, 66, 1) 100%);
-    font-weight: 600;
-    text-transform: uppercase;
-    color: white;
-}
+    .table th {
+        background: rgb(0, 27, 36);
+        background: radial-gradient(circle, rgba(0, 27, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(11, 7, 66, 1) 100%);
+        font-weight: 600;
+        text-transform: uppercase;
+        color: white;
+    }
 
-.table tbody tr:hover {
-    background-color: #f5f5f5;
-}
+    .table tbody tr:hover {
+        background-color: #f5f5f5;
+    }
 
-.panelDrop {
-    margin-left: 25%;
-    margin-top: 10px;
-}
+    .panelDrop {
+        margin-left: 25%;
+        margin-top: 10px;
+    }
 
-.arrow-icon {
-    height: 20px;
-    transition: transform 0.9s;
-}
+    .arrow-icon {
+        height: 20px;
+        transition: transform 0.9s;
+    }
 
-.dropdown:hover .arrow-icon {
-    transform: rotate(180deg);
-}
+    .dropdown:hover .arrow-icon {
+        transform: rotate(180deg);
+    }
 
-#serviceTypeSelect,
-#keywordsSelect {
-    background-color: #F3FDE8;
-    padding: 8px;
-    border-radius: 3%;
-}
+    #serviceTypeSelect,
+    #keywordsSelect {
+        background-color: #F3FDE8;
+        padding: 8px;
+        border-radius: 3%;
+    }
 
-#startDate {
-    padding: 7px;
-}
+    #startDate {
+        padding: 7px;
+    }
 </style>
 
 <div class="app-main__inner">
@@ -126,20 +126,20 @@ mysqli_close($connection);
                         <select id="serviceTypeSelect" name="service_type">
                             <option value="" disabled selected>Select Option</option>
                             <?php
-                                while ($row = mysqli_fetch_assoc($serviceResult)) {
-                                    echo '<option value="' . $row['service_type'] . '">' . $row['service_type'] . '</option>';
-                                }
-                                ?>
+                            while ($row = mysqli_fetch_assoc($serviceResult)) {
+                                echo '<option value="' . $row['service_type'] . '">' . $row['service_type'] . '</option>';
+                            }
+                            ?>
                         </select>
                     </div>
                     <div class="dropdown">
                         <select id="keywordsSelect" name="keyword">
                             <option value="" disabled selected>Select Option</option>
                             <?php
-                                while ($row = mysqli_fetch_assoc($keywordsResult)) {
-                                    echo '<option value="' . $row['keywords'] . '">' . $row['keywords'] . '</option>';
-                                }
-                                ?>
+                            while ($row = mysqli_fetch_assoc($keywordsResult)) {
+                                echo '<option value="' . $row['keywords'] . '">' . $row['keywords'] . '</option>';
+                            }
+                            ?>
                         </select>
                     </div>
                     <div class="dropdown">
@@ -185,37 +185,37 @@ mysqli_close($connection);
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
-$(document).ready(function() {
-    $("#filterButton").click(function() {
-        var selectedServiceType = $("#serviceTypeSelect").val();
-        var selectedKeywords = $("#keywordsSelect").val();
-        var selectedStartDate = $("#startDate").val();
+    $(document).ready(function() {
+        $("#filterButton").click(function() {
+            var selectedServiceType = $("#serviceTypeSelect").val();
+            var selectedKeywords = $("#keywordsSelect").val();
+            var selectedStartDate = $("#startDate").val();
 
-        $.ajax({
-            type: "GET",
-            url: "filter_data.php",
-            data: {
-                service_type: selectedServiceType,
-                keywords: selectedKeywords,
-                start_date: selectedStartDate
-            },
-            success: function(data) {
-                $("#tableBody").empty();
+            $.ajax({
+                type: "GET",
+                url: "filter_data.php",
+                data: {
+                    service_type: selectedServiceType,
+                    keywords: selectedKeywords,
+                    start_date: selectedStartDate
+                },
+                success: function(data) {
+                    $("#tableBody").empty();
 
-                data.forEach(function(row) {
-                    var newRow = "<tr>" +
-                        "<td>" + row.service_type + "</td>" +
-                        "<td>" + row.keywords + "</td>" +
-                        "<td>" + row.sms + "</td>" +
-                        "<td>" + row.datetime + "</td>" +
-                        "<td>" + row.status + "</td>" +
-                        "</tr>";
-                    $("#tableBody").append(newRow);
-                });
-            }
+                    data.forEach(function(row) {
+                        var newRow = "<tr>" +
+                            "<td>" + row.service_type + "</td>" +
+                            "<td>" + row.keywords + "</td>" +
+                            "<td>" + row.sms + "</td>" +
+                            "<td>" + row.datetime + "</td>" +
+                            "<td>" + row.status + "</td>" +
+                            "</tr>";
+                        $("#tableBody").append(newRow);
+                    });
+                }
+            });
         });
     });
-});
 </script>
 
 
--- include/header.php
+++ include/header.php
@@ -1,18 +1,19 @@
 <?php
 require_once("include/initialize.php");
-if($_SESSION['logged_in'] != true){
-  redirect_to("index.php");
+if ($_SESSION['logged_in'] != true) {
+    redirect_to("index.php");
 }
 
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Language" content="en">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Admin</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
     <meta name="description" content="This is an example dashboard created using build-in elements and components.">
@@ -27,9 +28,11 @@ if($_SESSION['logged_in'] != true){
     =========================================================
     * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
     -->
-<link href="themefiles/main.css" rel="stylesheet"></head>
+    <link href="themefiles/main.css" rel="stylesheet">
+</head>
 <link href="plugin/datatables.min.css" rel="stylesheet">
-<link href="css/dasstyle.css" rel="stylesheet"></head>
+<link href="css/dasstyle.css" rel="stylesheet">
+</head>
 <link href="plugin/dist/jquery-confirm.min.css" rel="stylesheet">
 <script type="text/javascript" src="themefiles/assets/scripts/main.js"></script>
 <script type="text/javascript" src="chart/canvasjs.min.js"></script>
@@ -48,7 +51,13 @@ if($_SESSION['logged_in'] != true){
     <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
         <div class="app-header header-shadow bg-grow-early  header-text-dark">
             <div class="app-header__logo">
-                <div class="logo-src"></div>
+                <div class="logo-src" style="font-size: 25px;color: white;font-weight: 700;margin-left: 32px;">
+                    <p style="margin-top: -13px;width: 142%;"><span>SMS Panel</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1418 125" fill="#000"style="margin-top:-32px;">
+                            <path d="M1412.29 72.17c-11.04-5.78-20.07-14.33-85.46-25.24-22.37-3.63-44.69-7.56-67.07-11.04-167.11-22.06-181.65-21.24-304.94-30.56C888.78 1.39 822.57 1.1 756.44 0c-46.63-.11-93.27 1.56-139.89 2.5C365.5 13.55 452.86 7.68 277.94 23.15 202.57 33.32 127.38 45.01 52.07 55.69c-11.23 2.41-22.63 4.17-33.71 7.22C6.1 66.33 5.64 66.19 3.89 67.79c-7.99 5.78-2.98 20.14 8.72 17.5 33.99-9.47 32.28-8.57 178.06-29.66 4.26 4.48 7.29 3.38 18.42 3.11 13.19-.32 26.38-.53 39.56-1.12 53.51-3.81 106.88-9.62 160.36-13.95 18.41-1.3 36.8-3.12 55.21-4.7 23.21-1.16 46.43-2.29 69.65-3.4 120.28-2.16 85.46-3.13 234.65-1.52 23.42.99 1.57-.18 125.72 6.9 96.61 8.88 200.92 27.94 295.42 46.12 40.87 7.91 116.67 23.2 156.31 36.78 3.81 1.05 8.28-.27 10.51-3.58 3.17-3.72 2.66-9.7-.78-13.13-3.25-3.12-8.14-3.44-12.18-5.08-17.89-5.85-44.19-12.09-63.67-16.56l26.16 3.28c23.02 3.13 46.28 3.92 69.34 6.75 10.8.96 25.43 1.81 34.34-4.39 2.26-1.54 4.86-2.75 6.21-5.27 2.76-4.59 1.13-11.06-3.59-13.68ZM925.4 23.77c37.64 1.4 153.99 10.85 196.64 14.94 45.95 5.51 91.89 11.03 137.76 17.19 24.25 4.77 74.13 11.21 101.72 18.14-11.87-1.15-23.77-1.97-35.65-3.06-133.46-15.9-266.8-33.02-400.47-47.21Z"></path>
+                        </svg>
+                    </p>
+                </div>
                 <div class="header__pane ml-auto">
                     <div>
                         <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
@@ -76,7 +85,8 @@ if($_SESSION['logged_in'] != true){
                         </span>
                     </button>
                 </span>
-            </div>    <div class="app-header__content">
+            </div>
+            <div class="app-header__content">
                 <div class="app-header-left">
                     <div class="search-wrapper">
                         <div class="input-holder">
@@ -85,29 +95,28 @@ if($_SESSION['logged_in'] != true){
                         </div>
                         <button class="close"></button>
                     </div>
-              </div>
+                </div>
                 <div class="app-header-right">
                     <div class="header-btn-lg pr-0">
                         <div class="widget-content p-0">
                             <div class="widget-content-wrapper">
                                 <div class="widget-content-left">
-                                   
- 
+
+
                                 </div>
                                 <div class="widget-content-left  ml-3 header-user-info">
                                     <div class="widget-heading">
-                                     <?php
-                                     if(isset($_SESSION['name'])){
-                                        echo $_SESSION['name'];
-                                     }
-                                     else{
+                                        <?php
+                                        if (isset($_SESSION['name'])) {
+                                            echo $_SESSION['name'];
+                                        } else {
 
-                                         echo "Admin";
-                                     }
+                                            echo "Admin";
+                                        }
 
-                                      ?>
+                                        ?>
                                     </div>
-                                    
+
                                 </div>
                                 <div class="widget-content-right header-user-info ml-3">
                                     <a href="index.php?logout=true" class="btn-shadow p-1 btn btn-primary btn-sm show-toastr-example">
@@ -116,46 +125,48 @@ if($_SESSION['logged_in'] != true){
                                 </div>
                             </div>
                         </div>
-                    </div>        </div>
-            </div>
-        </div>       
-       <div class="app-main">
-                <div class="app-sidebar sidebar-shadow bg-happy-green sidebar-text-light">
-                    <div class="app-header__logo">
-                        <div class="logo-src"></div>
-                        <div class="header__pane ml-auto">
-                            <div>
-                                <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
-                                    <span class="hamburger-box">
-                                        <span class="hamburger-inner"></span>
-                                    </span>
-                                </button>
-                            </div>
-                        </div>
                     </div>
-                    <div class="app-header__mobile-menu">
+                </div>
+            </div>
+        </div>
+        <div class="app-main">
+            <div class="app-sidebar sidebar-shadow bg-happy-green sidebar-text-light">
+                <div class="app-header__logo">
+                    <div class="logo-src"></div>
+                    <div class="header__pane ml-auto">
                         <div>
-                            <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
+                            <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
                                 <span class="hamburger-box">
                                     <span class="hamburger-inner"></span>
                                 </span>
                             </button>
                         </div>
                     </div>
-                    <div class="app-header__menu">
-                        <span>
-                            <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
-                                <span class="btn-icon-wrapper">
-                                    <i class="fa fa-ellipsis-v fa-w-6"></i>
-                                </span>
-                            </button>
-                        </span>
-                    </div>  
-
-
-                      <div class="scrollbar-sidebar">
-                        <div class="app-sidebar__inner">
-                            <?php include("nav.php"); ?>
-                        </div>
+                </div>
+                <div class="app-header__mobile-menu">
+                    <div>
+                        <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
+                            <span class="hamburger-box">
+                                <span class="hamburger-inner"></span>
+                            </span>
+                        </button>
                     </div>
-                </div>    <div class="app-main__outer">
+                </div>
+                <div class="app-header__menu">
+                    <span>
+                        <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
+                            <span class="btn-icon-wrapper">
+                                <i class="fa fa-ellipsis-v fa-w-6"></i>
+                            </span>
+                        </button>
+                    </span>
+                </div>
+
+
+                <div class="scrollbar-sidebar">
+                    <div class="app-sidebar__inner">
+                        <?php include("nav.php"); ?>
+                    </div>
+                </div>
+            </div>
+            <div class="app-main__outer">
