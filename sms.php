1681a869e5d79b68b732f75374da5e7f0b20dc18
sanad-bhowmik <sanad15-2952@diu.edu.bd>
2024-06-30 17:09:33 +0600

    update

--- sms.php
+++ sms.php
@@ -29,21 +29,27 @@ $result = $sql->get_result();
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        echo $row['sms'];
-if($telcoid==3){
-$registerMO_url='http://103.228.39.36/smppsend/blsmpp_test.php';
-$registerMO_param="?msisdn=$msisdn&message=".urlencode($row['sms'])."";
-$ftp222 =fopen("log/contentsms_".$date.".txt",'a+');
-        fwrite($ftp222,$registerMO_url."?".$registerMO_param."-".$datetime."\n");
-        fclose($ftp222);
-$response	=HttpRequest($registerMO_url,$registerMO_param);	
-}		
-
-
-		
+        //  echo $row['sms'];
+        if ($telcoid == 3 && $keyword != "" && $msisdn != "") {
+            $registerMO_url = 'http://103.228.39.36/smppsend/blsmpp_test.php';
+            $registerMO_param = "?msisdn=$msisdn&message=" . urlencode($row['sms']) . "";
+            $ftp222 = fopen("log/contentsms_" . $date . ".txt", 'a+');
+            fwrite($ftp222, $registerMO_url . "?" . $registerMO_param . "-" . $datetime . "\n");
+            fclose($ftp222);
+            try {
+               // $response    = HttpRequest($registerMO_url, $registerMO_param);
+               // echo 200;
+            } catch (Exception $e) {
+                echo 500;
+                $ftp222 = fopen("log/contentsms_" . $date . ".txt", 'a+');
+                fwrite($ftp222, $registerMO_url . "?" . $registerMO_param . "-" . $datetime . " error:" . $e . "\n");
+                fclose($ftp222);
+            }
+        }
     }
 } else {
-    echo "No SMS found for the keyword: " . htmlspecialchars($keyword);
+    //echo "No SMS found for the keyword: " . htmlspecialchars($keyword);
+    echo 404;
 }
 
 $conn->close();
@@ -51,16 +57,15 @@ $conn->close();
 
 
 
-function HttpRequest($url,$param) { 
-    $URL_STR =$url.$param;
-    $ch=curl_init();
-    curl_setopt($ch,CURLOPT_URL,$URL_STR);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER,false);
+function HttpRequest($url, $param)
+{
+    $URL_STR = $url . $param;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $URL_STR);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
     curl_exec($ch);
     $response = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     curl_close($ch);
     return $response;
-} 
-
-
+}
 ?>
